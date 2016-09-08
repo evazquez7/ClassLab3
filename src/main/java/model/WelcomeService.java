8da043ab2b4937952c4f7ea0580b860f89ea1cd2
Emilio <Emilio@10.30.11.251>
2016-09-07 19:17:23 -0500

    Started Lab 4

---
+++ src/main/java/model/WelcomeService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Emilio
+ */
+public class WelcomeService {
+    private Calendar currentTime = Calendar.getInstance();
+    private String timeOfDay;
+    private String MORNING = "Goodmorning";
+    private String NOON = "Afternoon";
+    private String NIGHT = "Goodnight";
+    
+     
+    private int beforeNoon = 12;
+    
+    
+    public String getTimeOfDay(){
+        
+        if (currentTime.before() > ){
+            
+        }
+        
+        return 
+    }
+    
+}
